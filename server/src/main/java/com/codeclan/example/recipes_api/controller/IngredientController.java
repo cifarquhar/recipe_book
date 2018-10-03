d6f414741c12c555f33441802eef8543c8023210
cifarquhar <colin.farquhar@codeclan.com>
2018-10-03 11:34:35 +0100

    add GET route to ingredient controller

--- server/src/main/java/com/codeclan/example/recipes_api/controller/IngredientController.java
+++ server/src/main/java/com/codeclan/example/recipes_api/controller/IngredientController.java
@@ -1,4 +1,24 @@
 package com.codeclan.example.recipes_api.controller;
 
+import com.codeclan.example.recipes_api.model.Ingredient;
+import com.codeclan.example.recipes_api.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IngredientController {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @GetMapping("/ingredients")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public Page<Ingredient> getIngredients(Pageable pageable){
+        return ingredientRepository.findAll(pageable);
+    }
+
 }
