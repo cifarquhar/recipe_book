4a226ac212768c25c5326be059b9f906216f6358
cifarquhar <colin.farquhar@codeclan.com>
2018-10-03 11:25:57 +0100

    add custom 404 handling

---
+++ server/src/main/java/com/codeclan/example/recipes_api/exception/EntryNotFoundException.java
@@ -0,0 +1,14 @@
+package com.codeclan.example.recipes_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntryNotFoundException extends RuntimeException{
+
+
+    public EntryNotFoundException(String message){
+        super(message);
+    }
+
+}
