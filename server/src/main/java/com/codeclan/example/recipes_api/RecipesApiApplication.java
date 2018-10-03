72c942bcf361f3267d115c3afdff93bc4a1f15a6
cifarquhar <colin.farquhar@codeclan.com>
2018-10-03 11:06:34 +0100

    initial commit, add bootstrapped spring project

---
+++ server/src/main/java/com/codeclan/example/recipes_api/RecipesApiApplication.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.recipes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesApiApplication.class, args);
+	}
+}
