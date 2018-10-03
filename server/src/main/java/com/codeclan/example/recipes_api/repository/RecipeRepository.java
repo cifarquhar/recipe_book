e2dbf265406472af1fe322834bce270e3ba535a7
cifarquhar <colin.farquhar@codeclan.com>
2018-10-03 11:23:18 +0100

    set up recipe repository

--- server/src/main/java/com/codeclan/example/recipes_api/repository/RecipeRepository.java
+++ server/src/main/java/com/codeclan/example/recipes_api/repository/RecipeRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.recipes_api.repository;
 
-public interface RecipeRepository {
+import com.codeclan.example.recipes_api.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 }
