10bec0338e14580fd1eec71791d2dedf76322114
cifarquhar <colin.farquhar@codeclan.com>
2018-10-25 16:26:31 +0100

    add and style navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+// import BeerList from "./components/BeerList";
+import NavBar from './components/NavBar';
+// import Favourites from "./components/Favourites";
+// import Add from "./components/Add";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <Router>
+
+        <div className="App">
+          <NavBar />
+
+          {/* <Route exact path="/" component={BeerList} /> */}
+          {/* <Route path="/favourites" component={Favourites} />
+          <Route path="/add" component={Add} /> */}
+
+        </div>
+      </Router>
     );
   }
 }
