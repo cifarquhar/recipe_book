bf060724ff5fa3724698c2b52a9dfcc28320cb17
cifarquhar <colin.farquhar@codeclan.com>
2018-10-25 16:30:43 +0100

    add content to home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-// import BeerList from "./components/BeerList";
 import NavBar from './components/NavBar';
+import Home from "./components/Home";
 // import Favourites from "./components/Favourites";
 // import Add from "./components/Add";
 
@@ -16,7 +16,7 @@ class App extends Component {
         <div className="App">
           <NavBar />
 
-          {/* <Route exact path="/" component={BeerList} /> */}
+          <Route exact path="/" component={Home} />
           {/* <Route path="/favourites" component={Favourites} />
           <Route path="/add" component={Add} /> */}
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Home extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <p>Welcome to the recipe book!</p>
+        <p>Still work in progress, some pretty pictures will go here later.</p>
+      </div>
+    )
+  }
+
+}
+
+export default Home;
