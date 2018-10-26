7631675d743845bcbafeec04a94b2308d4e77416
cifarquhar <colin.farquhar@codeclan.com>
2018-10-26 14:58:13 +0100

    add further details to modal

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -19,6 +19,12 @@ class Recipe extends React.Component{
     this.setState({ showModal: false });
   }
 
+  mapMethod(method) {
+    return Object.keys(method).map((step, index) => {
+      return <p key={index}>{step}: {method[step]}</p>
+    })
+  }
+
 
   render(){
     const recipe = this.props.recipe;
@@ -36,11 +42,17 @@ class Recipe extends React.Component{
             <p>{recipe.name}</p>
             <div style={{ display: "inline-flex" }}>
               <div className="ingredient-div">
-                {/* {this.mapIngredients(recipe.ingredients)} */}
+                <p>Serves: {recipe.servings}</p>
+                <p>Preparation Time: {recipe.prepTime} mins</p>
+                <p>Cooking Time: {recipe.cookTime} mins</p>
               </div>
               <div className="ingredient-div">
-                {/* {this.mapHops(recipe.hopSchedule)} */}
+                <p>Ingredients will go here</p>
               </div>
+            </div>
+            <div>
+              <p><strong>Method:</strong></p>
+              {this.mapMethod(recipe.method)}
             </div>
           </Modal>
         </div>
@@ -51,73 +63,3 @@ class Recipe extends React.Component{
 }
 
 export default Recipe;
-
-
-// class Beer extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showModal: false
-//     };
-//   }
-
-//   showModal() {
-//     this.setState({ showModal: true });
-//   }
-
-//   hideModal(event) {
-//     event.stopPropagation()
-//     this.setState({ showModal: false });
-//   }
-
-//   deleteBeer(event) {
-//     const p = this.props
-//     p.handleDelete(p.beer)
-//     this.hideModal(event)
-//   }
-
-//   mapIngredients(ingredients) {
-//     return Object.keys(ingredients).map((ingredient, index) => {
-//       return <p key={index}>{ingredient}: {ingredients[ingredient]}g</p>
-//     })
-//   }
-
-//   mapHops(hops) {
-//     let sortedHops = Object.keys(hops).sort((a, b) => hops[b] - hops[a])
-//     return sortedHops.map((hop, index) => {
-//       return <p key={index}>{hop}: {hops[hop] >= 0 ? `${hops[hop]} mins` : `after ${hops[hop] * -1} days`}</p>
-//     })
-//   }
-
-//   render() {
-//     const beer = this.props.beer;
-
-//     return (
-//       <div>
-//         <div className="beer-summary" onClick={this.showModal.bind(this)}>
-//           <div className="summary-header">
-//             <div className="summary-details">{beer.name}</div>
-//             <div className="summary-details">{beer.style}</div>
-//             <div className="favourite-logo"><Star beer={beer} /></div>
-//           </div>
-//           <p>{beer.description}</p>
-//           <Modal show={this.state.showModal} handleClose={(this.hideModal.bind(this))} handleDelete={this.deleteBeer.bind(this)} beerID={beer.id}>
-//             <p>{beer.name}</p>
-//             <div style={{ display: "inline-flex" }}>
-//               <div className="ingredient-div">
-//                 {this.mapIngredients(beer.ingredients)}
-//               </div>
-//               <div className="ingredient-div">
-//                 {this.mapHops(beer.hopSchedule)}
-//               </div>
-//             </div>
-//           </Modal>
-//         </div>
-//       </div>
-//     )
-//   }
-
-// }
-
-// export default Beer;
