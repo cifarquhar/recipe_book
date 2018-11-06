7a451a131781183895e542ba497871cb99c6617e
cifarquhar <colin.farquhar@codeclan.com>
2018-11-06 16:16:41 +0000

    add filtering of favourite recipes

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -7,7 +7,9 @@ class RecipeList extends React.Component {
     super(props);
     this.state = {
       isLoaded: false,
-      recipes: []
+      recipes: [],
+      filtered: false,
+      filteredRecipes: []
     };
   }
 
@@ -18,7 +20,11 @@ class RecipeList extends React.Component {
   callAPI() {
     fetch("http://localhost:8080/recipes")
       .then(response => response.json())
-      .then(data => this.setState({ isLoaded: true, recipes: this.mapRecipeData(data._embedded.recipes) }));
+      .then(data => this.setState({ 
+                          isLoaded: true, 
+                          recipes: this.mapRecipeData(data._embedded.recipes), 
+                          filteredRecipes: this.mapRecipeData(this.filterRecipes(data._embedded.recipes))
+                        }));
   }
 
   deleteRecipe(recipe) {
@@ -36,16 +42,29 @@ class RecipeList extends React.Component {
     }));
   }
 
+  filterRecipes(recipes){
+    return recipes.filter((recipe) => {
+      return recipe.favourite;
+    });
+  }
+
+  flipFilterState(){
+    this.setState({filtered: !this.state.filtered});
+  }
+
   render() {
-    const { isLoaded, recipes } = this.state;
+    const { isLoaded, recipes, filtered, filteredRecipes } = this.state;
 
     if (!isLoaded) {
       return <p>Waiting for recipe data...</p>
     }
 
+    const buttonText = filtered ? "Show all" : "Show favourites"
+
     return (
       <div>
-        {recipes}
+        <button onClick={this.flipFilterState.bind(this)}>{buttonText}</button>
+        {filtered ? filteredRecipes : recipes}
       </div>
     )
   }
